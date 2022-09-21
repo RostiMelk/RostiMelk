2b523e03d1b241e38a8fd0e5b47c2029929c6ea2
RostiMelk <hello@rosti.no>
2022-09-21 23:23:03 +0200

    Added vsco

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { getAccessToken } from '../lib/spotify';
 import portrait from '../public/portrait.jpg';
 import { Document, Header, Card, SpotifyCard } from '../components';
 import { RiLinkedinFill, RiMailFill, RiTwitterFill, RiGithubFill, RiInstagramLine } from 'react-icons/ri';
+import { SiVsco } from 'react-icons/si';
 
 export const getServerSideProps: GetServerSideProps = async () => {
 	const { access_token } = await getAccessToken();
@@ -33,6 +34,7 @@ const Home = ({ spotifyToken }: HomeProps) => {
 			<Card icon={RiTwitterFill} title="Twitter" href="https://twitter.com/rostimelk" />
 			<Card icon={RiGithubFill} title="GitHub" href="https://github.com/rostimelk" />
 			<Card icon={RiInstagramLine} title="Instagram" href="https://instagram.com/rostimelk" />
+			<Card icon={SiVsco} title="VSCO" href="https://vsco.co/rostimelk" />
 		</Document>
 	);
 };
