f09d6b8d8f46162b29da53bc8f8670df98f61bdb
RostiMelk <hello@rosti.no>
2022-09-22 12:57:01 +0200

    Hide personal social media urls if referer is work related

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,29 @@
 import type { GetServerSideProps } from 'next';
 import { getAccessToken } from '../lib/spotify';
+import { isWorkRelatedReferer } from '../lib/utils';
 import portrait from '../public/portrait.jpg';
 import { Document, Header, Card, SpotifyCard } from '../components';
 import { RiLinkedinFill, RiMailFill, RiTwitterFill, RiGithubFill, RiInstagramLine } from 'react-icons/ri';
 import { SiVsco } from 'react-icons/si';
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+	const isWorkRelated = isWorkRelatedReferer(ctx.req.headers.referer);
 	const { access_token } = await getAccessToken();
 
 	return {
 		props: {
+			isWorkRelated,
 			spotifyToken: access_token || null,
 		},
 	};
 };
 
 interface HomeProps {
+	isWorkRelated: boolean;
 	spotifyToken: string;
 }
 
-const Home = ({ spotifyToken }: HomeProps) => {
+const Home = ({ isWorkRelated, spotifyToken }: HomeProps) => {
 	return (
 		<Document>
 			<Header picture={portrait} pictureAlt="Portrait of Rostislav Melkumyan">
@@ -33,8 +37,12 @@ const Home = ({ spotifyToken }: HomeProps) => {
 			<Card icon={RiMailFill} title="Email" href="mailto:hello@rosti.no" />
 			<Card icon={RiTwitterFill} title="Twitter" href="https://twitter.com/rostimelk" />
 			<Card icon={RiGithubFill} title="GitHub" href="https://github.com/rostimelk" />
-			<Card icon={RiInstagramLine} title="Instagram" href="https://instagram.com/rostimelk" />
-			<Card icon={SiVsco} title="VSCO" href="https://vsco.co/rostimelk" />
+			{!isWorkRelated && (
+				<>
+					<Card icon={RiInstagramLine} title="Instagram" href="https://instagram.com/rostimelk" />
+					<Card icon={SiVsco} title="VSCO" href="https://vsco.co/rostimelk" />
+				</>
+			)}
 		</Document>
 	);
 };
