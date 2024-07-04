ae820247720d3d7d2ba6cb5ff2f610d79b372c8c
RostiMelk <hello@rosti.no>
2024-07-04 10:27:25 +0200

    fix: cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { SpotifySection } from '@/components/SpotifySection';
 import { LinksSection } from '@/components/LinksSection';
 import { Suspense } from 'react';
 
+export const fetchCache = 'force-no-store';
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen container max-w-2xl py-24 gap-20 flex-col relative">
